fc6daa5ae63819aafffc7254882653d8b2418aa8
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2026-03-20 13:24:41 +0530

    8

--- joke/src/App.tsx
+++ joke/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import { RandomJoke } from './components/random'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <RandomJoke />
     </>
   )
 }
